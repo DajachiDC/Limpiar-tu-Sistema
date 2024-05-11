2c0ad2d3ae5a6fad67cede34cce8a9ed21d6db4c
David <164821148+DajachiDC@users.noreply.github.com>
2024-05-11 13:17:29 -0500

    Add files via upload

---
+++ limpiar.sh
@@ -0,0 +1,11 @@
+echo ""
+echo "Script para optimizar tu sistema."
+echo "By: Dajachi"
+echo ""
+sudo apt autoclean
+sudo apt clean
+sudo apt autoremove
+sudo apt update
+sudo apt upgrade
+echo ""
+echo "Sistema optimizado correctamente."
